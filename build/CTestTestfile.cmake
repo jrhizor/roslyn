7dbdd33e6e7191083c602137787bdabfe53dbe74
Roslyn robot laptop <Roslyn robot laptop>
2013-12-09 19:38:29 -0800

    remove ros temp files

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rrl/roslyn/src
-# Build directory: /home/rrl/roslyn/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
-SUBDIRS(navigation)
--- build/navigation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rrl/roslyn/src/navigation
-# Build directory: /home/rrl/roslyn/build/navigation
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
