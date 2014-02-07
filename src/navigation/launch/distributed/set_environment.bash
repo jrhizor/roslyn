e0a5ae59100803ee17e1b0dade5b717f13d89d81
Tim Sweet <timothy.l.sweet@gmail.com>
2014-02-07 08:56:01 -0800

    Updated distributed launch

--- src/navigation/launch/distributed/set_environment.bash
+++ src/navigation/launch/distributed/set_environment.bash
@@ -6,9 +6,9 @@ export robot2_ip=10.8.2.22
 export robot2_path=/home/pi
 export robot2_user=pi
 
-export worker1_ip=10.8.2.6
-export worker1_path=/home/tim/
-export worker1_user=tim
+export worker1_ip=10.8.2.14
+export worker1_path=/home/rrl/
+export worker1_user=rrl
 
 export worker2_ip=10.8.2.18
 export worker2_path=/home/tim/
