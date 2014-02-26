5b78d39d3bf76a7cb81f885401798e228570add7
nyadav93 <nishokyadav@gmail.com>
2014-02-26 10:48:44 -0800

    updated interface design

--- tablet/assets/js/main.js
+++ tablet/assets/js/main.js
@@ -13,38 +13,23 @@ $(document).ready(function(){
     $("#welcome_button").show(); 
   };
   var enter_map_pane = function() {
-    $("#welcome_pane").fadeTo(200,0);
-    $("#interactive_mode_pane").fadeTo(200,0);
-    $("#map_pane").fadeTo(200,1);
+    $("#welcome_pane").fadeTo(400,0);
+    $("#interactive_mode_pane").hide();
+    $("#map_pane").fadeTo(400,1);
 
     hide_welcome();
 
     $("div").removeClass("current");
     $("#map_button").addClass("current");
 
-    $("body").css("background", "url('assets/img/map.png')");
+    $("body").css("background", "url('assets/img/map2.png')");
+
+
   };
 
-  var end_tour = function() {
-    $("#welcome_pane").fadeTo(200,1);
-    $("#map_pane").fadeTo(200,0);
-    $("#interactive_mode_pane").fadeTo(200,0);
-
-    show_welcome();
-
-    $("div").removeClass("current");
-    $("#welcome_button").addClass("current");
-
-    $("body").css("background", "#fff");
-  };
-
-  // handle buttons 
-  $("#begin_tour_button").click(enter_map_pane);
-  $("#map_button").click(enter_map_pane);
-
-  $("#interactive_mode_button").click(function(){
+  var enter_interactive_pane = function() {
     $("#welcome_pane").fadeTo(200,0);
-    $("#map_pane").fadeTo(200,0);
+    $("#map_pane").hide();
     $("#interactive_mode_pane").fadeTo(200,1);
 
     $("div").removeClass("current");
@@ -53,7 +38,26 @@ $(document).ready(function(){
     $("body").css("background", "url('assets/img/camera.png')");
   
     hide_welcome();
-  });
+
+  }
+
+  var end_tour = function() {
+    $("#welcome_pane").fadeTo(200,1);
+    $("#map_pane").hide();
+    $("#interactive_mode_pane").hide();
+
+    show_welcome();
+
+    $("div").removeClass("current");
+    $("#welcome_button").addClass("current");
+
+    $("body").css("background", "url(assets/img/binding_light.png)");
+  };
+
+  // handle buttons 
+  $("#begin_tour_button").click(enter_map_pane);
+  $("#map_button").click(enter_map_pane);
+  $("#interactive_mode_button").click(enter_interactive_pane);
 
   $("#end_tour_button").click(end_tour);
 
