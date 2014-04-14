394cd7516cc289069e2f99ab951d8aaf91eb40c2
nyadav93 <nishokyadav@gmail.com>
2014-04-13 21:01:19 -0700

    added clusters

--- tablet/assets/js/map.js
+++ tablet/assets/js/map.js
@@ -1,34 +1,36 @@
+var cluster = L.markerClusterGroup();
+
 //create an array to hold all the markers
 var offices = [
-   			//CSE Offices
-   			["<center><u>ECC</u> <br> The Engineering Computing Center is for the use of students that are pursuing a major or minor in the College of Engineering. " + 
+        //CSE Offices
+        ["<center><u>ECC</u> <br> The Engineering Computing Center is for the use of students that are pursuing a major or minor in the College of Engineering. " + 
         "It is also available to anyone that is currently enrolled in an engineering or computer science class. " +
         "<br> <a href='#ECC'>GO HERE</a> </center>", 0.268, 0.134],
-   			
-   			["<center><u>Computer Science and Engineering Department Office</u> <br>" +  
+        
+        ["<center><u>Computer Science and Engineering Department Office</u> <br>" +  
         "The Department of Computer Science and Engineering offers undergraduate and graduate degrees that provide a well-rounded education in computing. " +
         "Our students gain experience with both hardware and software and learn how to blend technical expertise with creative problem-solving " +
         "skills to push technological boundaries, create automated solutions to human problems and build better computing systems. " +
         "<br> <a href='#CSEClaytonOffice'>GO HERE</a> </center>", 0.252, 1.496],
 
-   			["<center><u>IT Offices</u> <br> <a href='#ITOffice'>GO HERE</a> </center>", 0.486, 1.570],
+        ["<center><u>IT Offices</u> <br> <a href='#ITOffice'>GO HERE</a> </center>", 0.486, 1.570],
             
         ["<center><u>Steve Clayton's Office</u> <br> IT Technician <br> <a href='#CSEClaytonOffice'>GO HERE</a> </center>",  0.280,  1.230],
-   			
-   			["<center><u>Dr. George Bebis's Office</u> <br> Dr. Bebis is the chair of the CSE department as well as a professor. " +
+        
+        ["<center><u>Dr. George Bebis's Office</u> <br> Dr. Bebis is the chair of the CSE department as well as a professor. " +
         "His research interests include: computer vision, image processing, machine learning, and pattern recognition. " +
         "His research has been funded by the NSF and NASA, among others. " +
         "<br> <a href='#CSEClaytonOffice'>GO HERE</a> </center>", 0.120, 1.650],
-   			
-   			["<center><u>Graduate Student Offices</u> <br> Graduate students have their offices in this area. " +
+        
+        ["<center><u>Graduate Student Offices</u> <br> Graduate students have their offices in this area. " +
         "Graduate students assist professors with the research they are working on. They may also act as teaching assistants (TAs) for classes. " +
         "<br> <a href='#GradOffices'>GO HERE</a> </center>", 0.909, -0.260],
-   			
-   			["<center><u>Dr. Fred Harris's Office</u> <br> Dr. Harris is the professor in charge of the High Performance Computation and Visualization (HPCVIS) Lab. " +
+        
+        ["<center><u>Dr. Fred Harris's Office</u> <br> Dr. Harris is the professor in charge of the High Performance Computation and Visualization (HPCVIS) Lab. " +
         "His research interests include: computer graphics, parallel computing, cortical simulation, and virtual reality. " +
         "<br> <a href='#HarrisNancyOffice'>GO HERE</a> </center>", -0.140, 1.650],
-   			
-   			["<center><u>Nancy LaTourette's Office</u> <br> Prof. LaTourette is a lecturer for the CSE department. Her research interests include: " +
+        
+        ["<center><u>Nancy LaTourette's Office</u> <br> Prof. LaTourette is a lecturer for the CSE department. Her research interests include: " +
         "discrete mathematics, theory of computation, and systems administration. " +
         "<br> <a href='#HarrisNancyOffice'>GO HERE</a> </center>", -0.140,  1.488],
             
@@ -83,21 +85,21 @@ var offices = [
 ];
 
 var labs = [
-   			//Labs
-   			["<center><u>Evolutionary Computing Systems Lab</u> <br> ECSL investigates systems that combine genetic algorithm search with case-based " +
+        //Labs
+        ["<center><u>Evolutionary Computing Systems Lab</u> <br> ECSL investigates systems that combine genetic algorithm search with case-based " +
         "reasoning principles. Such Case Injected Genetic Algorithms (CIGAR) lead to a new paradigm for machine learning " +
         "with special emphasis on design, optimization, and human modeling. " +
         "<br> <a href='#ECSL'>GO HERE</a> </center>", -0.570, -1.697],
-   			
-   			["<center><u>Computer Networking Lab</u> <br> The research efforts in the CNL are on various topics in computer communication networks " +
+        
+        ["<center><u>Computer Networking Lab</u> <br> The research efforts in the CNL are on various topics in computer communication networks " +
         "encompassing both experimental and theoretical aspects. Research performed in the CNL has been funded by both the government and by the industry. " +
         "<br> <a href='#NetworkLab'>GO HERE</a> </center>", 0.000, -1.690],
-   			
-   			["<center><u>Robotics Lab</u> <br> Dr. David Feil-Seifer and Dr. Monica Nicolescu run the Robotics Lab. " +
+        
+        ["<center><u>Robotics Lab</u> <br> Dr. David Feil-Seifer and Dr. Monica Nicolescu run the Robotics Lab. " +
         "Some of the research interests of these professors include: socially assistive robotics, artificial intelligence, " +
         "human-robot interaction, and behavior-based control. <br> <a href='#RoboticsLab'>GO HERE</a> </center>", 0.679, 1.570],
-   			
-   			["<center><u>Robotics Lab Workroom</u> <br> This room is currently used by undergraduate researchers who are working on senior projects related to robotics " +
+        
+        ["<center><u>Robotics Lab Workroom</u> <br> This room is currently used by undergraduate researchers who are working on senior projects related to robotics " +
         "<br> <a href='#RoboticsWorkroom'>GO HERE</a> </center>", 0.570, 1.230],
             
         ["<center><u>Cyber Infrastructure Lab</u> <br> The focused of the CIL is on developing new computing environments and " +
@@ -106,10 +108,10 @@ var labs = [
 ];
 
 var classrooms = [
-   			//Classrooms  			
-   			["<center>Classroom <br> <a href='#SushilOfficeClass'>GO HERE</a> </center>", 0.178, 0.909],
+        //Classrooms        
+        ["<center>Classroom <br> <a href='#SushilOfficeClass'>GO HERE</a> </center>", 0.178, 0.909],
 
-   			["<center>Engineering Grid Node <br> <a href='#GridNode'>GO HERE</a> </center>", -0.820, -1.700],
+        ["<center>Engineering Grid Node <br> <a href='#GridNode'>GO HERE</a> </center>", -0.820, -1.700],
             
         ["<center>Classroom <br> <a href='#Class2'>GO HERE</a> </center>",  0.250, -0.740],
             
@@ -117,28 +119,29 @@ var classrooms = [
 ];
 
 var amenities = [
-   			//Amenities
-   			["<center>Men's Restroom <br> <a href='#MenRR2'>GO HERE</a> </center>", 0.135, -0.443],
-   			["<center>Men's Restroom <br> <a href='#MenRR1'>GO HERE</a> </center>", 0.125, 1.230],
+        //Amenities
+        ["<center>Men's Restroom <br> <a href='#MenRR2'>GO HERE</a> </center>", 0.135, -0.443],
+        ["<center>Men's Restroom <br> <a href='#MenRR1'>GO HERE</a> </center>", 0.125, 1.230],
         ["<center>Women's Restroom <br> <a href='#WomenRR1'>GO HERE</a> </center>", -0.140,  0.550],
 ];    
 
-
-
+// create the map
 var map = L.map('map_pane', {zoom:8, minZoom:8, maxZoom:10, zoomControl:false}).setView([0,0], 0);
 
+// assign the bounds for the image
 var southWest = L.latLng(-400/360.0, -640/360.0),
 northEast = L.latLng(400/360.0, 640/360.0),
-bounds = L.latLngBounds(southWest, northEast);
+boundsImage = L.latLngBounds(southWest, northEast);
 
-L.imageOverlay('assets/img/scaleMap.png', bounds).addTo(map);
+// set the background image for the map, 1920x1200
+L.imageOverlay('assets/img/scaleMap.png', boundsImage).addTo(map);
 
-//sets bounds
+//sets bounds for the map
 var southWest2 = L.latLng(-500/360.0, -740/360.0),
 northEast2 = L.latLng(500/360.0, 740/360.0),
-bounds2 = L.latLngBounds(southWest2, northEast2);
+boundsMap = L.latLngBounds(southWest2, northEast2);
 
-map.setMaxBounds(bounds2);
+map.setMaxBounds(boundsMap);
 
 var greenIcon = L.icon({
     iconUrl: 'assets/img/marker-icon-green.png',
@@ -189,38 +192,46 @@ var yellowIcon = L.icon({
 for (var i = 0; i < offices.length; i++) 
 {
 	marker = new L.marker([offices[i][1],offices[i][2]], {icon: blueIcon})
-		.bindPopup(offices[i][0])
-		.addTo(map);
+		.bindPopup(offices[i][0]);
+	
+  cluster.addLayer(marker);
+  map.addLayer(cluster);
 }
 
 //Place all lab markers on the map
 for (var i = 0; i < labs.length; i++) 
 {
-   marker = new L.marker([labs[i][1],labs[i][2]], {icon: redIcon})
-      .bindPopup(labs[i][0])
-      .addTo(map);
+  marker = new L.marker([labs[i][1],labs[i][2]], {icon: redIcon})
+      .bindPopup(labs[i][0]);
+
+  cluster.addLayer(marker);
+  map.addLayer(cluster);
 }
 
 //Place all classroom markers on the map
 for (var i = 0; i < classrooms.length; i++) 
 {
-   marker = new L.marker([classrooms[i][1],classrooms[i][2]], {icon: greenIcon})
-      .bindPopup(classrooms[i][0])
-      .addTo(map);
+  marker = new L.marker([classrooms[i][1],classrooms[i][2]], {icon: greenIcon})
+      .bindPopup(classrooms[i][0]);
+
+  cluster.addLayer(marker);
+  map.addLayer(cluster);
 }
 
 //Place all amenities markers on the map
 for (var i = 0; i < amenities.length; i++) 
 {
-   marker = new L.marker([amenities[i][1],amenities[i][2]], {icon: yellowIcon})
-      .bindPopup(amenities[i][0])
-      .addTo(map);
+  marker = new L.marker([amenities[i][1],amenities[i][2]], {icon: yellowIcon})
+      .bindPopup(amenities[i][0]);
+
+  cluster.addLayer(marker);
+  map.addLayer(cluster);
 }
 
-// //(just for testing) create a popup whereever the user clicks the map
-// var popup = L.popup();
-// function onMapClick(e) {
-// popup.setLatLng(e.latlng).setContent("You clicked the map at " + e.latlng.toString()).openOn(map);
-// }
+//(just for testing) create a popup whereever the user clicks the map
+var popup = L.popup();
+function onMapClick(e) {
+popup.setLatLng(e.latlng).setContent("You clicked the map at " + e.latlng.toString()).openOn(map);
+}
 
-// map.on('click', onMapClick);
+map.on('click', onMapClick);
