8f01e3b51c9b982876b8a1a537fb364ff9a0da19
nyadav93 <nishokyadav@gmail.com>
2014-05-01 18:10:15 -0700

    updated files for use at lawlor

---
+++ app/www/assets/js/mainLawlor.js
@@ -0,0 +1,387 @@
+/*
+  TODO:
+  [x] add all locations  
+  [x] add all destinations for locations
+  [x] add descriptions for locations
+  [x] make groupings
+  [ ] make multiple colored icons for markers
+  [ ] send destination to ROS
+  [ ] get current location from ROS and display it
+  [ ] get video feed and display it
+        http://www.html5rocks.com/en/tutorials/getusermedia/intro/
+  [ ] display points of interest in interactive mode
+  [ ] send terminate tour with end tour in ROS
+  [ ] package with Adobe Cordova
+  [ ] test on actual tour
+  [ ] add appropriate distance from human
+*/
+
+var marker;
+
+var convertTFtoLeaflet = function(x,y) {
+  a =  0.0006;
+  b =  0.0310;
+  c =  0.7145;
+  d =  0.0338;
+  e =  0.0002;
+  f =  1.2579;
+  g =  0.0007;
+  h = -0.0007;
+  i =  1.0000;
+
+  x_prime = a*x + b*y + c;
+  y_prime = d*x + e*y + f;
+  w_prime = g*x + h*y + i;
+
+  return [x_prime/w_prime, y_prime/w_prime];
+};
+
+// var convertLeaflettoTF = function(x,y) {
+//   var x_prime = (x * -282.354631) + (y * 461.703328);
+//   var y_prime = (x * -179.848807) + (y * 294.326665);
+//   return [x_prime, y_prime];
+// };
+
+var destinations = 
+{
+	//    leaflet locations in hall [y,x]
+  // "ECC": [0.00275, 0.12634],
+  // "CSEClaytonOffice": [0.28839, 1.35132],
+  // "ITOffice": [0.49301, 1.35132],
+  // "RoboticsLab": [0.71272, 1.35132],
+  // "RoboticsWorkroom": [0.75254, 1.230],
+  // "MenRR1": [0.00549, 1.230],
+  // "WomenRR1": [0.00824, 0.550],
+  // "HarrisNancyOffice": [0.00824, 1.44196],
+  // "MonicaOffice": [0.00824, 1.350],
+  // "GunesOffice": [0.00824, 1.210],
+  // "YukselOffice": [0.00824, 1.080],
+  // "DascaluOffice": [0.00824, 0.950],
+  // "SushilOfficeClass": [0.00824, 0.675],
+  // "MirceaOffice": [0.00824, 0.440],
+  // "MichaelOffice": [0.00824, 0.315],
+  // "CyberLab": [0.75254, -0.457],
+  // "GradOffices": [0.75254, -0.26],
+  // "Class1": [0.56853, -0.56442],
+  // "Class2": [0.21423, -0.56442],
+  // "MenRR2": [0.00824, -0.443],
+  // "WomenRR2": [0.00824, -1.480],
+  // "DaveOffice": [0.00824, -0.925],
+  // "VarolOffice": [0.00824,-1.050],
+  // "NetworkLab": [0.08377, -1.58066],
+  // "EelkeOffice": [-0.280, -1.58752],
+  // "BryantOffice": [-0.400, -1.5889],
+  // "ShamikOffice": [-0.520, -1.59439],
+  // "GridNode": [-0.820, -1.59851],
+  // "ECSL": [-0.570,-1.59439]
+
+
+  //ros locations [x,y]
+  //labs
+  "poster": [],    
+  "lawlorEntrance": [], 
+  "lawlorOffice": [], 
+  "posterRoom": []
+  
+}
+
+function onDeviceReady()
+{
+ //  alert('onDeviceReady')
+ 
+
+ // if(typeof navigator.camera != 'undefined')
+ //  {
+ //    alert('oh yes1')
+ //  }
+ //  else
+ //  {
+ //    alert('oh no1')
+ //  }
+
+
+ //  if(typeof navigator.camera.getPicture != 'undefined')
+ //  {
+ //    alert('oh yes')
+ //  }
+ //  else
+ //  {
+ //    alert('oh no')
+ //  }
+
+  // if (navigator.camera == null) {
+  //   location.reload();
+  // }
+
+  // if (!navigator.Camera) {
+  //     alert("Camera API not supported", "Error");
+  //   }
+  //   else
+  //   {
+  //   var options =   {   quality: 50,
+  //                       de//stinationType: Camera.DestinationType.DATA_URL,
+  //                       sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Album
+  //                       encodingType: 0     // 0=JPG 1=PNG
+  //                   };
+
+  //   navigator.camera.getPicture(
+  //       function(imageData) {
+  //           $('.employee-image', this.el).attr('src', "data:image/jpeg;base64," + imageData);
+  //       },
+  //       function() {
+  //           alert('Error taking picture', 'Error');
+  //       },
+  //       options);  
+  //   }
+
+}
+
+$(document).ready(function(){
+
+
+//document.addEventListener("deviceready",onDeviceReady,false);
+
+// handle ROS connection
+var ros = new ROSLIB.Ros({url:'ws://10.8.4.1:9090'});
+// var ros = new ROSLIB.Ros({url:'ws://127.0.0.1:9090'});
+
+// If there is an error on the backend, an 'error' emit will be emitted.
+ros.on('error', function(error) {
+  console.log(error);
+});
+
+// Find out exactly when we made a connection.
+ros.on('connection', function() {
+  console.log('Connection made!');
+});
+
+// LISTENER
+var listener = new ROSLIB.Topic({
+  ros : ros,
+  name : '/amcl_pose',
+  messageType : 'geometry_msgs/PoseWithCovarianceStamped'
+});
+
+var tf_listener = new ROSLIB.TFClient({
+  ros : ros,
+  fixedFrame: 'map',
+  angularThres : 0.001,
+  transThres : 0.001
+});
+
+
+
+// Then we add a callback to be called every time a message is published on this topic.
+listener.subscribe(function(message) {
+  // for (var key in message.pose) {
+  // // do something with key
+  //   console.log(key + ' ' + message.pose[key]);
+  // } 
+
+  var x = message.pose.pose.position.x;
+  var y = message.pose.pose.position.y;
+  // console.log(message);
+  // $("#map_robot_marker").css({'top': Math.floor(y*100+300).toString()+'px',
+  //                             'left': Math.floor(x*100+300).toString()+'px'
+  //                           });
+  // $("#map_robot_marker").set('left', floor(message.pose.position.x*1000+300).toString()+'px');
+
+
+});
+
+
+tf_listener.subscribe('base_link', function(tf) {
+  //console.log(tf_listener.processFeedback(tf));
+  //console.log(tf);
+  // console.log(tf.translation.x, tf.translation.y);
+  // console.log(convertTFtoLeaflet(tf.translation.x, tf.translation.y));
+  // console.log('-------')
+  
+  map.removeLayer(marker);
+
+ //[0.01648, -1.38977
+
+  current_loc.setLatLng(convertTFtoLeaflet(tf.translation.x, tf.translation.y));
+
+  // map.removeLayer(marker);
+  // marker = new L.marker(convertTFtoLeaflet(tf.translation.x, tf.translation.y), {icon: redIcon})
+  //     .addTo(map);
+
+});
+
+
+
+
+
+
+  // initialize
+  $("#end_tour_button").hide();
+
+  // functions to handle styling
+  var hide_welcome = function() { 
+    $("#welcome_button").hide(); 
+    $("#end_tour_button").show(); 
+  };
+  var show_welcome = function() { 
+    $("#end_tour_button").hide(); 
+    $("#welcome_button").show(); 
+  };
+  var enter_map_pane = function() {
+    $("#welcome_pane").fadeTo(400,0);
+    $("#begin_tour_button").hide();
+    $("#interactive_mode_pane").hide();
+    $("#map_pane").fadeTo(400,1);
+
+    hide_welcome();
+
+    $("div").removeClass("current");
+    $("#map_button").addClass("current");
+
+
+  };
+
+  var enter_interactive_pane = function() {
+    $("#welcome_pane").fadeTo(200,0);
+    $("#map_pane").hide();
+    $("#interactive_mode_pane").fadeTo(200,1);
+
+    $("div").removeClass("current");
+    $("#interactive_mode_button").addClass("current");
+  
+    hide_welcome();
+
+  }
+
+
+  var submitEndTour = new ROSLIB.Topic({
+    ros : ros,
+    name : '/move_base/cancel',
+    messageType : 'actionlib_msgs/GoalID'
+  });
+
+  var end_tour = function() {
+
+
+    var newGoal = new ROSLIB.Message({
+      stamp : {},
+      id : ""
+    });
+
+
+    submitEndTour.publish(newGoal);
+
+    setTimeout(function(){location.reload()},100)
+
+    map.setView([0,0], 0);
+    map.closePopup();
+
+    $("#welcome_pane").fadeTo(200,1);
+    $("#end_tour_pane").fadeTo(200, 0);
+    $("#begin_tour_button").show();
+    $("#map_pane").hide();
+    $("#interactive_mode_pane").hide();
+
+    show_welcome();
+
+    $("div").removeClass("current");
+    $("#welcome_button").addClass("current");
+
+    document.body.style.background="url('assets/img/binding_light.png')";
+
+  };
+
+  // handle buttons 
+  $("#begin_tour_button").click(enter_map_pane);
+  $("#map_button").click(enter_map_pane);
+  $("#interactive_mode_button").click(enter_interactive_pane);
+
+  $("#end_tour_button").click(end_tour);
+
+
+
+  var submitGoal = new ROSLIB.Topic({
+    ros : ros,
+    name : '/move_base_simple/goal',
+    messageType : 'geometry_msgs/PoseStamped'
+  });
+
+  $(window).bind( 'hashchange', function(e) { 
+    if(window.location.hash)
+    {
+      // TODO CONVERT THIS-21.9127
+      var converted = destinations[window.location.hash.substring(1)];
+      console.log(destinations[window.location.hash.substring(1)])
+      console.log(converted);
+
+      // RoboticsWorkroom
+      var robotics_lab_x = 0.154026;      
+      var robotics_lab_y = 0.278556;
+
+      // ECC
+      var ecc_x = -34.403;      
+      var ecc_y = -21.9127;
+
+      // 
+
+      var x;
+      var y;
+
+      if(window.location.hash=="#ECC")
+      {
+        x = ecc_x;
+        y = ecc_y;
+      }
+      else if(window.location.hash=="#RoboticsWorkroom")
+      {
+        x = robotics_lab_x;
+        y = robotics_lab_y;
+      }
+
+      var newGoal = new ROSLIB.Message({
+        header : {
+          seq : 1,
+          stamp : {
+            secs : 0,
+            nsecs : 0
+          },
+          frame_id : 'map'
+        },
+        pose : {
+          position : {
+            x : converted[0],
+            y : converted[1],
+            z : 0
+          },
+          orientation : {
+            x : 0,
+            y : 0,
+            z : 0,
+            w : 1
+          }
+        }
+      });
+      submitGoal.publish(newGoal);
+
+
+
+      //alert('go to ' + window.location.hash + ' ' + destinations[window.location.hash.substring(1)])
+      window.location.hash = '#';
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
---
+++ app/www/assets/js/mapLawlor.js
@@ -0,0 +1,158 @@
+var cluster = L.markerClusterGroup({showCoverageOnHover: false, maxClusterRadius: 80});
+
+//create arrays to store the different types of points of interests
+var poster = 
+        ["<center><b>Roslyn Project Poster</b> <br>" +
+         "The team poster for our project provides more detail about our project." + 
+         "There should be at least one team member nearby in case you have any questions." +
+        "<br> <a href='#poster'>GO HERE</a> </center>", -0.3955, 0.1181];
+
+var posterRoom =
+        [   "<center><b>Innovation Day Poster Room </b><br>" +
+            "Senior capstone projects represent innovative engineering products " +
+            "designed by teams of undergraduate students during the course of their senior year." +
+            "<br> <a href='#posterRoom'>GO HERE</a></center>", -0.32135, 0.64819];
+
+var lawlorOffice =
+        [   "<center><b>Lawlor Events Center Offices</b><br>" +
+            "Offices used by Lawlor Events Center." +
+            "<br> <a href='#lawlorOffice'>GO HERE</a></center>", -0.222, 1.057];
+                
+var entrance =
+        [   "<center><b>Lawlor Events Center Entrance</b><br>" +
+            "Lawlor Events Center houses many different types of events. These include: " +
+            "Senior Capstone Innovation Day, basketball games, concerts, and university ceremonies." +
+            "<br> <a href='#lawlorEntrance'>GO HERE</a></center>", -0.994, 1.032];
+
+var hall =
+        [   "<center><b>Main Hall</b><br>" +
+            "This is the main hall around the main event arena. I cannot take you here, but if you get a chance you might " +
+            "notice an awesome wolf in the case. GO PACK!"
+            , 0.589, 0.664];
+
+// create the map
+var map = L.map('map_pane', {
+  zoom:8, 
+  minZoom:8, 
+  maxZoom:10, 
+  zoomControl:false})
+  .setView([0,0], 0);
+
+// assign the bounds for the image
+var southWest = L.latLng(-600/360.0, -960/360.0),
+northEast = L.latLng(600/360.0, 960/360.0),
+boundsImage = L.latLngBounds(southWest, northEast);
+
+// set the background image for the map, 1920x1200
+L.imageOverlay('assets/img/roslynInterfaceMap.png', boundsImage).addTo(map);
+
+//sets bounds for the map
+var southWest2 = L.latLng(-350/360.0, -550/360.0),  
+northEast2 = L.latLng(350/360.0, 500/360.0),        
+boundsMap = L.latLngBounds(southWest2, northEast2);
+
+map.setMaxBounds(boundsMap);
+
+// Set parameters for the different icons
+var greenIcon = L.icon({
+    iconUrl: 'assets/img/marker-icon-green.png',
+    iconRetinaUrl: 'assets/img/marker-icon-2x-green.png',
+    iconSize: [25, 41],
+    iconAnchor: [22, 51],
+    popupAnchor: [1, -34],
+    shadowUrl: 'assets/img/marker-shadow.png',
+    shadowSize: [41, 41],
+    shadowAnchor: [12.5, 41]
+});
+
+var blueIcon = L.icon({
+    iconUrl: 'assets/img/marker-icon-blue.png',
+    iconRetinaUrl: 'assets/img/marker-icon-2x-blue.png',
+    iconSize: [25, 41],
+    iconAnchor: [22, 51],
+    popupAnchor: [1, -34],
+    shadowUrl: 'assets/img/marker-shadow.png',
+    shadowSize: [41, 41],
+    shadowAnchor: [12.5, 41]
+});
+
+var redIcon = L.icon({
+    iconUrl: 'assets/img/marker-icon-red.png',
+    iconRetinaUrl: 'assets/img/marker-icon-2x-red.png',
+    iconSize: [25, 41],
+    iconAnchor: [22, 51],
+    popupAnchor: [1, -34],
+    shadowUrl: 'assets/img/marker-shadow.png',
+    shadowSize: [41, 41],
+    shadowAnchor: [12.5, 41]
+});
+
+var purpleIcon = L.icon({
+    iconUrl: 'assets/img/marker-icon-purple.png',
+    iconRetinaUrl: 'assets/img/marker-icon-2x-purple.png',
+    iconSize: [25, 41],
+    iconAnchor: [22, 51],
+    popupAnchor: [1, -34],
+    shadowUrl: 'assets/img/marker-shadow.png',
+    shadowSize: [41, 41],
+    shadowAnchor: [12.5, 41]
+});
+
+var wolfIcon = L.icon({
+    iconUrl: 'assets/img/wolf.png',
+    iconSize: [40, 30],
+    iconAnchor: [30, 40],
+    popupAnchor: [0, -30],
+});
+
+var currentLocIcon = L.icon({
+    iconUrl: 'assets/img/markerCurrentLoc.png',
+    iconSize: [25, 41],
+    iconAnchor: [22, 51],
+    popupAnchor: [1, -34],
+    shadowUrl: 'assets/img/marker-shadow.png',
+    shadowSize: [41, 41],
+    shadowAnchor: [12.5, 41]
+});
+
+
+//Place team poster marker
+marker = new L.marker([poster[1], poster[2]], {icon: blueIcon}).bindPopup(poster[0]);
+cluster.addLayer(marker);
+map.addLayer(cluster);
+
+//Place poster room marker
+marker = new L.marker([posterRoom[1], posterRoom[2]], {icon: greenIcon}).bindPopup(posterRoom[0]);
+cluster.addLayer(marker);
+map.addLayer(cluster);
+
+//Place lawlor offices marker
+marker = new L.marker([lawlorOffice[1], lawlorOffice[2]], {icon: purpleIcon}).bindPopup(lawlorOffice[0]);
+cluster.addLayer(marker);
+map.addLayer(cluster);
+
+//Place lawlor entrance marker
+marker = new L.marker([entrance[1], entrance[2]], {icon: redIcon}).bindPopup(entrance[0]);
+cluster.addLayer(marker);
+map.addLayer(cluster);
+
+//Place hall marker
+marker = new L.marker([hall[1], hall[2]], {icon: wolfIcon}).bindPopup(hall[0]);
+cluster.addLayer(marker);
+map.addLayer(cluster);
+
+var current_loc = new L.marker([-0.29,0.14395], {icon: currentLocIcon});
+map.addLayer(current_loc);
+
+//(just for testing) create a popup whereever the user clicks the map
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        //.setContent("<center>Click on any marker to see a description of that place. <br>If you want to go to the selected destination, <br> press 'GO HERE' </center>")
+        .setContent(e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
